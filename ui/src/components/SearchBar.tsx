a2a5d0dab5954301809635537a49f7a71f4d9baa
Alex McLeod <alexwillmcleod@gmail.com>
2023-07-27 18:07:21 +1200

    fix: fixed search bar button

--- ui/src/components/SearchBar.tsx
+++ ui/src/components/SearchBar.tsx
@@ -42,36 +42,34 @@ export default function SearchBar({
   // );
 
   return (
-    <div class="md:form-control">
-      <div class="md:input-group">
-        <input
-          type="search"
-          class="input input-bordered md:join-item w-96"
-          name="search"
-          value={unsubmittedSearchTerm()}
-          onInput={handleUnsubmittedSearchTermChange}
-          placeholder="Search for something tasty..."
-        />
-        <button
-          onClick={() => handleSearchSubmit(unsubmittedSearchTerm())}
-          class="md:flex hidden btn md:btn-square"
+    <div class="md:input-group">
+      <input
+        type="search"
+        class="input input-bordered md:join-item w-96"
+        name="search"
+        value={unsubmittedSearchTerm()}
+        onInput={handleUnsubmittedSearchTermChange}
+        placeholder="Search for something tasty..."
+      />
+      <button
+        onClick={() => handleSearchSubmit(unsubmittedSearchTerm())}
+        class="md:btn md:btn-square md:cursor-pointer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="cursor-pointer h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-      </div>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
